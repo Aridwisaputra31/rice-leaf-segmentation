2f1c63ec5163aee05863cbcd7aa59f9ac67f47f7
Aridwisaputra31 <dwissaputraari@gmail.com>
2026-02-20 10:43:52 +0700

    Update segmentation_step_2.py

--- src/segmentation_step_2.py
+++ src/segmentation_step_2.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import os
 
 ddir=os.listdir(r'D:\File Photo\Ari\SAMSUNG 1\HST 60\Baris 6 hst 60 ari\Equalisasi\Hasil_Clahe\Masking1')
-#print(ddir)
 folderFile=r'D:\File Photo\Ari\SAMSUNG 1\HST 60\Baris 6 hst 60 ari\Equalisasi\Hasil_Clahe\Masking1'
 folsave=r'D:\File Photo\Ari\SAMSUNG 1\HST 60\Baris 6 hst 60 ari\Equalisasi\Hasil_Clahe\Masking2'
 
@@ -12,14 +11,12 @@ for i in ddir:
     if i.endswith('jpg'):
         print(i)
         nfol_hasil=os.path.join(folderFile,i)
-        #baca citra dengan format RGB agar tampilan cocok dengan matplotlib
         img_0 = cv2.imread(nfol_hasil) #RGB
         img_rgb=cv2.cvtColor(img_0, cv2.COLOR_BGR2RGB)
         img_hsv=cv2.cvtColor(img_0, cv2.COLOR_BGR2HSV)
         
-        #tentukan range nilai piksel untuk mask
-        lower_green=np.array([21,30,48],dtype=np.uint8) #lab
-        upper_green=np.array([73,238,236],dtype=np.uint8) #S 238 jgn di ubah
+        lower_green=np.array([21,30,48],dtype=np.uint8) 
+        upper_green=np.array([73,238,236],dtype=np.uint8) 
         mask=cv2.inRange(img_hsv,lower_green,upper_green)
         
         iterasi=6
@@ -34,7 +31,6 @@ for i in ddir:
         eroded1 = cv2.erode(closing1, kernel1, iterations=iterasi)
         
         hasil_rgb=cv2.bitwise_and(img_rgb,img_rgb, mask=eroded1)
-
         
         parts = i.split('_')
         nf_hasil1 = "Hasil2_" + '_'.join(parts[1:])
